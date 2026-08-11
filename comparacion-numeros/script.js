5b73a8765482c40ffeb9f46ea95091ca4eff6ecd
trescuerpos <kevin.navarrogrz@gmail.com>
2026-08-11 17:06:34 -0600

    Primer desafio

---
+++ comparacion-numeros/script.js
@@ -0,0 +1,19 @@
+import PromptSync from "prompt-sync";
+const prompt = PromptSync();
+
+
+function numberOrder(a, b, c) {
+
+    let numeros = [a, b, c];
+
+    numeros.sort((a, b) => a - b);
+
+    console.log(numeros);
+
+}
+
+let num1 = Number(prompt("Ingresa el primer número:"));
+let num2 = Number(prompt("Ingresa el segundo número:"));
+let num3 = Number(prompt("Ingresa el tercer número:"));
+
+numberOrder(num1, num2, num3);
